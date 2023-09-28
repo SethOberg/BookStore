2450841a3c09db4bff53dcd74249f3fbdacbb95f
sethoberg <seob95@gmail.com>
2023-09-28 11:29:05 +0200

    Feat: add models

---
+++ BookStore/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+	public class Author
+	{
+		[Key]
+		public int Id { get; set; }
+		public string FirstName { get; set; } = null;
+		public string LastName { get; set; } = null;
+		public ICollection<Book> Books { get; set; } = null;
+
+		public Author()
+		{
+		}
+	}
+}
+
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Models
+{
+	public class Book
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int PageCount { get; set; }
+		[Column(TypeName = "decimal(6,2)")]
+		public decimal Price { get; set; }
+		public DateTime Published { get; set; }
+        public int QuantityInStock { get; set; }
+        public ICollection<Author> Authors { get; set; } = null;
+
+        public Book()
+		{
+		}
+	}
+}
+
