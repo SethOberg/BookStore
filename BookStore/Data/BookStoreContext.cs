3cae347071932fb1962db5c3c1e87edfea0de46d
sethoberg <seob95@gmail.com>
2023-10-01 17:09:16 +0200

    Feat: add db context object

--- BookStore/Data/BookStoreContext.cs
+++ BookStore/Data/BookStoreContext.cs
@@ -6,12 +6,18 @@ namespace BookStore.Data
 {
 	public class BookStoreContext : DbContext
 	{
-		public DbSet<Author> Authors { get; set; }
-		public DbSet<Book> Books { get; set; }
+		public DbSet<Author> Authors { get; set; } = null;
+		public DbSet<Book> Books { get; set; } = null;
+		public DbSet<AuthorOfBook> AuthorOfBooks { get; set; } = null;
 
 		public BookStoreContext()
 		{
 		}
-	}
+
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+    }
 }
 
