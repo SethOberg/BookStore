37135abb739a615aa00c9d258bfaacd7acd6a7f3
sethoberg <seob95@gmail.com>
2023-10-01 17:09:00 +0200

    Feat: add models

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -8,13 +8,13 @@ namespace BookStore.Models
 	{
 		[Key]
 		public int Id { get; set; }
-		public string Title { get; set; }
-		public int PageCount { get; set; }
+		public string Title { get; set; } = null;
+		public int? PageCount { get; set; }
 		[Column(TypeName = "decimal(6,2)")]
-		public decimal Price { get; set; }
-		public DateTime Published { get; set; }
-        public int QuantityInStock { get; set; }
-        public ICollection<Author> Authors { get; set; } = null;
+		public decimal? Price { get; set; }
+		public DateTime? Published { get; set; }
+        public int? QuantityInStock { get; set; }
+        public ICollection<Author> Authors { get; set; } = new List<Author>();
 
         public Book()
 		{
