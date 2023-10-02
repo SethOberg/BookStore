2fcc64c74acc1c62e97f66814a5e90c99cf8a6ab
sethoberg <seob95@gmail.com>
2023-10-02 13:04:57 +0200

    Feat: add Razor pages for CRUD operations

---
+++ BookStore/Pages/Authors/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Models;
+
+namespace BookStore.Pages.Authors
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly BookStore.Data.BookStoreContext _context;
+
+        public DeleteModel(BookStore.Data.BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Author Author { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Authors == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Author = author;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Authors == null)
+            {
+                return NotFound();
+            }
+            var author = await _context.Authors.FindAsync(id);
+
+            if (author != null)
+            {
+                Author = author;
+                _context.Authors.Remove(Author);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
