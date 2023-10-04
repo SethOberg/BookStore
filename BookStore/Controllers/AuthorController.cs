0cf2c8d6372f2543d2bbd75add04e2eaa3577ffb
sethoberg <seob95@gmail.com>
2023-10-04 20:24:57 +0200

    Chore: fix formatting

--- BookStore/Controllers/AuthorController.cs
+++ BookStore/Controllers/AuthorController.cs
@@ -32,23 +32,23 @@ namespace BookStore.Controllers
             }
 
             var authorsWithBooks = await _context.Authors
-      .Include(a => a.Books)
-      .Select(a => new AuthorWithBooksDTO
-      {
-          AuthorId = a.Id,
-          FirstName = a.FirstName,
-          LastName = a.LastName,
-          Books = a.Books.Select(b => new BookDTO
-          {
-              BookId = b.Id,
-              Title = b.Title,
-              PageCount = b.PageCount,
-              Price = b.Price,
-              Published = b.Published,
-              QuantityInStock = b.QuantityInStock
-          }).ToList()
-      })
-      .ToListAsync();
+                .Include(a => a.Books)
+                .Select(a => new AuthorWithBooksDTO
+                {
+                    AuthorId = a.Id,
+                    FirstName = a.FirstName,
+                    LastName = a.LastName,
+                    Books = a.Books.Select(b => new BookDTO
+                    {
+                        BookId = b.Id,
+                        Title = b.Title,
+                        PageCount = b.PageCount,
+                        Price = b.Price,
+                        Published = b.Published,
+                        QuantityInStock = b.QuantityInStock
+                    }).ToList()
+                })
+                .ToListAsync();
 
             return authorsWithBooks;
         }
@@ -58,10 +58,10 @@ namespace BookStore.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<AuthorWithBooksDTO>> GetAuthor(int id)
         {
-          if (_context.Authors == null)
-          {
-              return NotFound();
-          }
+            if (_context.Authors == null)
+            {
+                return NotFound();
+            }
 
             var author = await _context.Authors
           .Where(a => a.Id == id)
@@ -126,10 +126,10 @@ namespace BookStore.Controllers
         [HttpPost]
         public async Task<ActionResult<Author>> PostAuthor(Author author)
         {
-          if (_context.Authors == null)
-          {
-              return Problem("Entity set 'BookStoreContext.Authors'  is null.");
-          }
+            if (_context.Authors == null)
+            {
+                return Problem("Entity set 'BookStoreContext.Authors'  is null.");
+            }
             _context.Authors.Add(author);
             await _context.SaveChangesAsync();
 
