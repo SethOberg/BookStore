38c7189c6b8ebf5ecc9231ba6aa13ee1553dd8e8
sethoberg <seob95@gmail.com>
2023-10-13 16:59:22 +0200

    Feat: add API controllers for User and Order

--- BookStore/Data/BookStoreContext.cs
+++ BookStore/Data/BookStoreContext.cs
@@ -8,6 +8,8 @@ namespace BookStore.Data
 	{
 		public DbSet<Author> Authors { get; set; } = null;
 		public DbSet<Book> Books { get; set; } = null;
+		public DbSet<User> Users { get; set; } = null;
+		public DbSet<Order> Orders { get; set; } = null;
 
 		public BookStoreContext()
 		{
